a01cf4bf22c5a9b8b16c52011cad958fcb48dbcd
Thomas Pontikes <tapontikes@gmail.com>
2019-07-28 20:47:20 -0400

    Added heroku docker

--- src/start.ts
+++ src/start.ts
@@ -1,4 +1,8 @@
 import AppServer from './server';
 
 const appServer = new AppServer();
-appServer.start(3000);
+const nodeEnvPort: string = (process.env.NODE_ENV as string);
+
+const port = Number(nodeEnvPort) || 3000;
+
+appServer.start(port);
